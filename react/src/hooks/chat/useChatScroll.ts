6ca5e2172edc7ea067bca710cb93920ef9d03d06
Apolos666 <quangprox121@gmail.com>
2025-04-02 19:17:10 +0700

    Enhance useChatScroll hook: add support for message ID tracking and smooth scrolling to new messages.

--- react/src/hooks/chat/useChatScroll.ts
+++ react/src/hooks/chat/useChatScroll.ts
@@ -1,10 +1,10 @@
 import {useEffect, useRef} from "react";
 
-function UseChatScroll<T>(dependency : T[]) {
+function UseChatScroll<T extends { id: string }>(dependency: T[]) {
     const bottomScrollRef = useRef<HTMLSpanElement>(null)
     const scrollAreaRef = useRef<HTMLDivElement>(null)
-
-    const hasScrolledToBottom = useRef(false);
+    const hasScrolledToBottom = useRef(false)
+    const lastMessageRef = useRef<string | null>(null)
 
     // This effect will run when the component mounts and scroll to the bottom
     useEffect(() => {
@@ -14,6 +14,22 @@ function UseChatScroll<T>(dependency : T[]) {
         }
     }, [])
 
+    // Scroll to bottom when new messages arrive, but not when loading history.
+    // The reason for this to work is, the messages I already reversed, so the most recent is at the end
+    useEffect(() => {
+        const mostRecentMessage = dependency[dependency.length - 1]
+
+        if (hasScrolledToBottom.current &&
+            lastMessageRef.current !== null &&
+            mostRecentMessage.id !== lastMessageRef.current)
+        {
+            bottomScrollRef.current?.scrollIntoView({behavior: 'smooth'});
+        }
+
+        lastMessageRef.current = mostRecentMessage.id
+    }, [dependency]);
+
+    // Helper function to scroll down a bit when loading new messages
     const scrollTop = () => {
         if (scrollAreaRef.current) {
             // Get the actual viewport element inside ScrollArea
@@ -24,7 +40,7 @@ function UseChatScroll<T>(dependency : T[]) {
         }
     }
 
-    return { bottomScrollRef, scrollAreaRef, scrollTop }
+    return {bottomScrollRef, scrollAreaRef, scrollTop}
 }
 
 export default UseChatScroll;
